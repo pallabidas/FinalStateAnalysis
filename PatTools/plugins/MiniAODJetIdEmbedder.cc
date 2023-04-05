b8492134a7dc6d1f974e8e07451f4fe44d3a4f3b
pallabidas <pallabi91@gmail.com>
2023-04-05 23:21:38 +0200

    updates for adding gen di-b matching

--- PatTools/plugins/MiniAODJetIdEmbedder.cc
+++ PatTools/plugins/MiniAODJetIdEmbedder.cc
@@ -13,6 +13,9 @@
 #include "FWCore/Framework/interface/EDProducer.h"
 
 #include "DataFormats/PatCandidates/interface/Jet.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+#include "DataFormats/Math/interface/deltaR.h"
+#include "TLorentzVector.h"
 
 class MiniAODJetIdEmbedder : public edm::EDProducer {
   public:
@@ -21,10 +24,12 @@ class MiniAODJetIdEmbedder : public edm::EDProducer {
     void produce(edm::Event& evt, const edm::EventSetup& es);
   private:
     edm::EDGetTokenT<edm::View<pat::Jet> > srcToken_;
+    edm::EDGetTokenT<edm::View<reco::GenParticle> > genParticlesToken_;
 };
 
 MiniAODJetIdEmbedder::MiniAODJetIdEmbedder(const edm::ParameterSet& pset) {
   srcToken_ = consumes<edm::View<pat::Jet> >(pset.getParameter<edm::InputTag>("src"));
+  genParticlesToken_ = consumes<edm::View<reco::GenParticle> >(pset.getParameter<edm::InputTag>("genParticles"));
   produces<pat::JetCollection>();
 }
 
@@ -35,6 +40,34 @@ void MiniAODJetIdEmbedder::produce(edm::Event& evt, const edm::EventSetup& es) {
   evt.getByToken(srcToken_, input);
 
   output->reserve(input->size());
+
+  edm::Handle<edm::View<reco::GenParticle> > genPart;
+  evt.getByToken(genParticlesToken_, genPart);
+  TLorentzVector v1;
+  TLorentzVector v2;
+  //TLorentzVector higgs;
+  for ( auto& genp : *genPart ) {
+    //if(genp.pdgId() == 25 && genp.status() == 62) higgs.SetPtEtaPhiM(gen.pt(), genp.eta(), genp.phi(), 125.09);
+    if(genp.pdgId() == 5 && genp.status() == 23) v1.SetPtEtaPhiM(genp.pt(), genp.eta(), genp.phi(), 4.18);
+    if(genp.pdgId() == -5 && genp.status() == 23) v2.SetPtEtaPhiM(genp.pt(), genp.eta(), genp.phi(), 4.18);
+  }
+  size_t matchedindex = 99;
+  float dR_genBs = v1.DeltaR(v2);
+  if(dR_genBs < 0.8){
+    TLorentzVector diB = v1 + v2;
+    size_t jetindex = 0;
+    edm::View<pat::Jet>::const_iterator jet;
+    float deltar = 0.4;
+    const edm::View<pat::Jet> & jets = *input;
+    for(jet = jets.begin(); jet != jets.end(); ++jet){
+      if(reco::deltaR(jet->eta(), jet->phi(), diB.Eta(), diB.Phi()) < deltar){
+        deltar = reco::deltaR(jet->eta(), jet->phi(), diB.Eta(), diB.Phi());
+        matchedindex = jetindex;
+      }
+      jetindex++;
+    }
+  }
+
   for (size_t i = 0; i < input->size(); ++i) {
     pat::Jet jet = input->at(i);
     // https://twiki.cern.ch/twiki/bin/view/CMS/JetID
@@ -172,6 +205,13 @@ void MiniAODJetIdEmbedder::produce(edm::Event& evt, const edm::EventSetup& es) {
       }
 
     jet.addUserFloat("puID", float(passPU));
+
+
+    // Add matching to merged gen bs
+    bool passMatching = false;
+    if(i == matchedindex) passMatching = true;
+
+    jet.addUserFloat("matchesGenBB", float(passMatching));
     output->push_back(jet);
   }
 
